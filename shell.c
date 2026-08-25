0f2ff1d3c63a40a76706db5cb644113687d2622e
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-08-25 07:33:52 +0000

    Support arguments for simple shell command execution

--- shell.c
+++ shell.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * simple_shell - Reads and executes single-word commands using execve
+ * simple_shell - Reads and executes commands with arguments using execve
  */
 void simple_shell(void)
 {
@@ -9,8 +9,8 @@ void simple_shell(void)
 	size_t len = 0;
 	ssize_t read;
 	pid_t pid;
-	char *argv[2];
-	char *cmd;
+	char *argv[1024];
+	char *token;
 	int i;
 
 	while (1)
@@ -26,43 +26,27 @@ void simple_shell(void)
 			break;
 		}
 
-		/* إزالة حرف السطر الجديد والأطراف الفارغة */
-		for (i = 0; line[i] != '\0'; i++)
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		/* تقسيم السطر المدخل إلى كلمات ومعاملات باستخدام strtok */
+		i = 0;
+		token = strtok(line, " \t");
+		while (token != NULL)
 		{
-			if (line[i] == '\n' || line[i] == '\r')
-			{
-				line[i] = '\0';
-				break;
-			}
+			argv[i++] = token;
+			token = strtok(NULL, " \t");
 		}
+		argv[i] = NULL;
 
-		/* تجاوز المسافات في البداية */
-		cmd = line;
-		while (*cmd == ' ' || *cmd == '\t')
-			cmd++;
-
-		if (*cmd == '\0')
+		/* إذا كان السطر فارغاً، استمر */
+		if (i == 0)
 			continue;
-
-		/* إزالة المسافات من النهاية */
-		for (i = strlen(cmd) - 1; i >= 0; i--)
-		{
-			if (cmd[i] == ' ' || cmd[i] == '\t')
-				cmd[i] = '\0';
-			else
-				break;
-		}
-
-		if (strlen(cmd) == 0)
-			continue;
-
-		argv[0] = cmd;
-		argv[1] = NULL;
 
 		pid = fork();
 		if (pid == -1)
 		{
-			perror("Error forging process");
+			perror("Error forking process");
 			exit(EXIT_FAILURE);
 		}
 		if (pid == 0)
